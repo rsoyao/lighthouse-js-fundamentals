6cbe1689e300e8d617d14d2998102ff70529a4d2
Ram Soyao <r.soyao@live.com>
2018-04-09 11:43:37 -0400

    adding new excercises

---
+++ loopy-lighthouse.js
@@ -0,0 +1,17 @@
+
+for (var numbers = 100; numbers <= 200; numbers++); {
+  if (numbers % 4 ===0 && numbers % 3 ===0){
+  console.log("Loopy Lighthouse");
+  }
+  else if (numbers % 3 ===0){
+  console.log("Loopy");
+  }
+  else if (numers % 4 ===0){
+  console.log("Lighthouse");
+  }
+  else {
+    console.log(numbers);
+  }
+}
+
+
---
+++ loopy-lighthouse2.js
@@ -0,0 +1,4 @@
+function concat(series1, series2) {
+  var series3 = series1.concat(series2);
+  return series3;
+}
